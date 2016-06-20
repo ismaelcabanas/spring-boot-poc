936d598d10cff2c2091ee652e42996eb362dce6c
Ismael Cabanas Garcia <Ismael Cabanas Garcia>
2016-06-20 18:39:12 +0200

    Inicialización del proyecto

---
+++ src/main/java/cabanas/garcia/ismael/springbootpoc/SpringBootPocApplication.java
@@ -0,0 +1,12 @@
+package cabanas.garcia.ismael.springbootpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPocApplication.class, args);
+	}
+}
