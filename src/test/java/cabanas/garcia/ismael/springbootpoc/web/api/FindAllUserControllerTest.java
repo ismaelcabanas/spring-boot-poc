b88848df8bc3e500f38c3074eef5cc543862db7d
Ismael Cabanas Garcia <Ismael Cabanas Garcia>
2016-06-22 17:46:27 +0200

    Nuevos tests unitarios para el endpoint api/user
    Del test unitario inicial se han extraido dos nuevos tests unitarios para
    que cada test compruebe una cosa específica. Se ha refactorizado el nombre
    de la clase para facilitar la agrupación de tests unitarios.

---
+++ src/test/java/cabanas/garcia/ismael/springbootpoc/web/api/FindAllUserControllerTest.java
@@ -0,0 +1,119 @@
+package cabanas.garcia.ismael.springbootpoc.web.api;
+
+import cabanas.garcia.ismael.springbootpoc.model.User;
+import cabanas.garcia.ismael.springbootpoc.service.UserService;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Unit tests for method GET of endpoint /api/user.
+ * 
+ * Created by ismael on 21/06/2016.
+ */
+public class FindAllUserControllerTest extends AbstractControllerTest {
+
+    private static final String USER_NAME = "UserName Test";
+
+    private static final String ENDPOINT = "/api/user";
+
+    @Mock
+    private UserService userMockService;
+
+    @InjectMocks
+    private UserController controller;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        setUp(controller);
+    }
+
+    @Test
+    public void should_call_findAll_user_service_when_endpoint_api_user() throws Exception{
+
+        // given
+
+        // when
+        mvc.perform(
+                get(ENDPOINT)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON_UTF8)
+                )
+                .andDo(log());
+
+        // then
+        Mockito.verify(userMockService, VerificationModeFactory.times(1)).findAll();
+        Mockito.verifyNoMoreInteractions(userMockService);
+
+    }
+
+    @Test
+    public void should_ok_response_when_endpoint_api_user() throws Exception{
+
+        // given
+
+        // when
+        mvc.perform(
+                get(ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+                )
+                .andExpect(status().isOk())
+                .andDo(log());
+
+    }
+
+
+    @Test
+    public void should_list_users_endpoint_api_user() throws Exception{
+
+        // given
+        Collection<User> users = getUsersStubData();
+        Mockito.when(userMockService.findAll()).thenReturn(users);
+
+        // when
+        mvc.perform(
+                get(ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"name\":\"UserName Test\"}]"))
+                .andDo(log());
+
+    }
+
+    private Collection<User> getUsersStubData() {
+        Collection<User> list = new ArrayList<User>();
+        list.add(getUserStubData());
+        return list;
+    }
+
+    private User getUserStubData() {
+        User user = new User.UserBuilder()
+                .name(USER_NAME)
+                .build();
+        return user;
+    }
+
+}
