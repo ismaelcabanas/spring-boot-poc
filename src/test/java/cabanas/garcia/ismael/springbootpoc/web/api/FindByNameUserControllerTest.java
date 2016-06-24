28354ac71dd963da98693212c60d9675f447ac13
Ismael Cabanas Garcia <Ismael Cabanas Garcia>
2016-06-24 12:36:38 +0200

    Nuevos tests unitarios para UserController
    Creada una nueva clase que tendrá tests unitarios para el endpoint
    /api/user/{name}. El primer test unitario comprueba que el estado de la
    respuesta es un 200 cuando se llama al endpoint.

--- src/test/java/cabanas/garcia/ismael/springbootpoc/web/api/FindByNameUserControllerTest.java
+++ src/test/java/cabanas/garcia/ismael/springbootpoc/web/api/FindByNameUserControllerTest.java
@@ -1,9 +1,44 @@
 package cabanas.garcia.ismael.springbootpoc.web.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
 /**
- * Unit tests for method GET of endpoint /api/user/.
+ * Unit tests for method GET of endpoint /api/user/{name}.
  *
  * Created by ismael on 23/06/2016.
  */
-public class FindByNameUserControllerTest {
+public class FindByNameUserControllerTest extends AbstractControllerTest {
+
+    private static final String ENDPOINT = "/api/user/{name}";
+
+    @InjectMocks
+    private UserController controller;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        super.setUp(controller);
+    }
+
+    @Test
+    public void should_return_200_status_code_when_call_endpoint() throws Exception{
+
+        // given
+        String userName = "test name";
+
+        // when
+        mvc.perform(get(ENDPOINT, userName))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(log());
+
+    }
+
 }
