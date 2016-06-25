25fdc6ceedc8632d416f0901931abd8bf32e2cd1
Ismael Cabanas Garcia <Ismael Cabanas Garcia>
2016-06-25 09:17:39 +0200

    Refactor

--- src/test/java/cabanas/garcia/ismael/springbootpoc/web/api/FindByNameUserControllerTest.java
+++ src/test/java/cabanas/garcia/ismael/springbootpoc/web/api/FindByNameUserControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.MockitoAnnotations;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 /**
